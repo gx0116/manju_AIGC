9227dd5f5f34e3b2224c686faca32df2633e4ab3
hgxxx <15662504+hgxxx@user.noreply.gitee.com>
2026-05-17 17:04:54 +0800

    基本模块构建

---
+++ src/main/java/com/hgx/manju_aigc/ManjuAigcApplication.java
@@ -0,0 +1,13 @@
+package com.hgx.manju_aigc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManjuAigcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManjuAigcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hgx/manju_aigc/ManjuAigcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgx.manju_aigc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManjuAigcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
